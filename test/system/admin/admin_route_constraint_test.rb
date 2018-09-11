8794b1bc5d7ccab12ee7578416d7dfe7243f4a86
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2018-09-11 16:19:28 +0300

    Add test for the route constraint
    It only works with Rack::Test driver for capybara, that is probably
    somehow related to how threading in tests works. It however, does not
    make too much sense yet to introduce separate integration test class.

--- test/system/admin/admin_route_constraint_test.rb
+++ test/system/admin/admin_route_constraint_test.rb
@@ -1,8 +1,38 @@
 require 'application_system_test_case'
 
-
 class AdminRouteConstraintTest < ApplicationSystemTestCase
-  def test_cannot_see_the_route_as_user
-    skip("Needs to be researched, no traditional method of testing it, like assert_raises work")
+  def setup
+    super
+    Capybara.current_driver = :rack_test
+  end
+
+  def teardown
+    super
+    Capybara.current_driver = :headless_chrome
+  end
+
+  def test_cannot_see_the_routes_as_user
+    user = users(:user)
+    sign_in(user)
+
+    assert_raise(ActionController::RoutingError) do
+      visit admin_users_path
+    end
+  end
+
+  def test_cannot_see_the_routes_when_not_signed_in
+    assert_raise(ActionController::RoutingError) do
+      visit admin_users_path
+    end
+  end
+
+  def test_can_see_the_routes_as_administrator
+    user = users(:administrator)
+    sign_in(user)
+
+    visit admin_users_path
+
+    assert_text('Joe John User')
+    assert_text('John Joe Administrator')
   end
 end
--- test/system/admin/users/admin_users_profile_test.rb
+++ test/system/admin/users/admin_users_profile_test.rb
@@ -1,6 +1,6 @@
 require 'application_system_test_case'
 
-class AdminUsersEditTest < ApplicationSystemTestCase
+class AdminUsersProfileTest < ApplicationSystemTestCase
   def setup
     super
     @user = users(:user)
