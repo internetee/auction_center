f6e6b320cb19e4b8438abe85bf8d108b3093242b
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2018-09-24 13:48:05 +0300

    Add links to the navbar

--- app/controllers/admin/versions_controller.rb
+++ app/controllers/admin/versions_controller.rb
@@ -1,9 +1,10 @@
 module Admin
   class VersionsController < BaseController
     def index
-      resource_class, @resource_id = request.path.split('/')[2, 2]
-      @audit_class = ('Audit::' + resource_class.singularize.classify).constantize
-      @items = @audit_class.where(object_id: @resource_id).order(recorded_at: :desc)
+      resource_class, resource_id = request.path.split('/')[2, 2]
+      audit_class = ('Audit::' + resource_class.singularize.classify).constantize
+
+      @items = audit_class.where(object_id: resource_id).order(recorded_at: :desc)
     end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,17 +3,36 @@ module ApplicationHelper
     Rails.configuration.customization['application_name']
   end
 
-  def sign_in_button(current_user)
-    return if current_user
-    content_tag(:li, class: 'nav-item') do
-      link_to(t(:sign_in), new_user_session_path, class: 'nav-link')
+  def sign_in_links(current_user)
+    content_tag(:ul, class: 'navbar-nav mt-2 my-lg-0') do
+      if current_user
+        content_tag(:li, class: 'nav-item') do
+          link_to(t(:sign_out), destroy_user_session_path, class: 'nav-link', method: :delete)
+        end
+      else
+        content_tag(:li, class: 'nav-item') do
+          link_to(t(:sign_in), new_user_session_path, class: 'nav-link')
+        end
+      end
     end
   end
 
-  def sign_out_button(current_user)
-    return unless current_user
-    content_tag(:li, class: 'nav-item') do
-      link_to(t(:sign_out), destroy_user_session_path, class: 'nav-link', method: :delete)
+  def navigation_links(current_user)
+    content_tag(:ul, class: 'navbar-nav mt-2 my-lg-0') do
+      if current_user
+        concat(
+          content_tag(:li, class: 'nav-item') do
+            link_to(t(:profile), user_path(current_user), class: 'nav-link')
+          end
+        )
+      end
+      if current_user&.role?(User::ADMINISTATOR_ROLE)
+        concat(
+          content_tag(:li, class: 'nav-item') do
+            link_to(t(:users_name), admin_users_path, class: 'nav-link')
+          end
+        )
+      end
     end
   end
 end
