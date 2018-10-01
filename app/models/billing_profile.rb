6a5a3b02aa8f83f18112de605f9c9f93573c0fa7
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2018-10-01 16:52:33 +0300

    Add required constraints for the first test to pass

--- app/models/billing_profile.rb
+++ app/models/billing_profile.rb
@@ -1,2 +1,8 @@
 class BillingProfile < ApplicationRecord
+  belongs_to :user, required: true
+
+  validates :street, presence: true
+  validates :city, presence: true
+  validates :postal_index, presence: true
+  validates :country, presence: true
 end
