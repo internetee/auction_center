3aef1be525fe190de6a713a685213bad65dbe431
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2018-11-15 15:38:25 +0200

    Attempt at fixing rubocop issues

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,34 +19,41 @@ module ApplicationHelper
 
   def navigation_links(current_user)
     content_tag(:ul, class: 'navbar-nav mt-2 my-lg-0') do
-      if current_user
-        links = [{ name: t(:profile), path: user_path(current_user) },
-                 { name: t(:my_offers), path: offers_path }]
-
-        links.each do |item|
-          concat(
-            content_tag(:li, class: 'nav-item') do
-              link_to(item[:name], item[:path], class: 'nav-link')
-            end
-          )
-        end
-      end
-
-      if current_user&.role?(User::ADMINISTATOR_ROLE)
-        links = [{ name: t(:auctions_name), path: admin_auctions_path },
-                 { name: t(:results_name), path: admin_results_path },
-                 { name: t(:billing_profiles_name), path: admin_billing_profiles_path },
-                 { name: t(:users_name), path: admin_users_path },
-                 { name: t(:settings_name), path: admin_settings_path }]
-
-        links.each do |item|
-          concat(
-            content_tag(:li, class: 'nav-item') do
-              link_to(item[:name], item[:path], class: 'nav-link')
-            end
-          )
-        end
-      end
+      links(user_link_list) if current_user
+      links(administrator_link_list) if current_user&.role?(User::ADMINISTATOR_ROLE)
     end
+  end
+
+  private
+
+  def links(links_list)
+    links_list.each do |item|
+      concat(
+        content_tag(:li, class: 'nav-item') do
+          link_to(item[:name], item[:path], class: 'nav-link')
+        end
+      )
+    end
+  end
+
+  def administrator_links
+    administrator_links_list.each do |item|
+      concat(content_tag(:li, class: 'nav-item') do
+               link_to(item[:name], item[:path], class: 'nav-link')
+             end)
+    end
+  end
+
+  def user_link_list
+    [{ name: t(:profile), path: user_path(current_user) },
+     { name: t(:my_offers), path: offers_path }]
+  end
+
+  def administrator_link_list
+    [{ name: t(:auctions_name), path: admin_auctions_path },
+     { name: t(:results_name), path: admin_results_path },
+     { name: t(:billing_profiles_name), path: admin_billing_profiles_path },
+     { name: t(:users_name), path: admin_users_path },
+     { name: t(:settings_name), path: admin_settings_path }]
   end
 end
