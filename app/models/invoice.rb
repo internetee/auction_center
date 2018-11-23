0450a680ee554f27d9f89e843895a7919618c0c3
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2018-11-23 16:43:29 +0200

    Add basic validations for invoice relations

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,1 +1,15 @@
-class Invoice < ApplicationRecord; end
+class Invoice < ApplicationRecord
+  belongs_to :result, required: true
+  belongs_to :user, required: false
+  belongs_to :billing_profile, required: true
+
+  validate :user_id_must_be_the_same_as_on_billing_profile_or_nil
+
+  def user_id_must_be_the_same_as_on_billing_profile_or_nil
+    return unless billing_profile
+    return if billing_profile.user_id == user_id
+    return unless user_id
+
+    errors.add(:billing_profile, I18n.t('invoices.billing_profile_must_belong_to_user'))
+  end
+end
---
+++ test/models/invoice_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class InvoiceTest < ActiveSupport::TestCase
+  def setup
+    super
+
+    @result = results(:expired_participant)
+    @user = users(:participant)
+    @orphan_billing_profile = billing_profiles(:orphan)
+    @company_billing_profile = billing_profiles(:company)
+  end
+
+  def test_required_fields
+    invoice = Invoice.new
+    refute(invoice.valid?)
+
+    assert_equal(['must exist'], invoice.errors[:result])
+    assert_equal(['must exist'], invoice.errors[:billing_profile])
+
+    invoice.result = @result
+    invoice.billing_profile = @orphan_billing_profile
+
+    assert(invoice.valid?)
+  end
+
+  def test_user_must_be_the_same_as_the_one_on_billing_or_nil
+    invoice = Invoice.new
+    invoice.result = @result
+    invoice.billing_profile = @orphan_billing_profile
+    invoice.user = @user
+
+    refute(invoice.valid?)
+    assert_equal(['must belong to the same user as invoice'], invoice.errors[:billing_profile])
+
+    invoice.user = nil
+    assert(invoice.valid?)
+  end
+end
