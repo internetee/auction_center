e416d42d61f8d5a322f5011618d9a966949f4532
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2019-01-23 13:49:19 +0200

    Remove obosolete src folder

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,3 +12,21 @@ import Rails from 'rails-ujs';
 import Turbolinks from 'turbolinks';
 Rails.start();
 Turbolinks.start();
+
+// Semantic UI
+import '../src/semantic-ui/definitions/modules/transition.js';
+import '../src/semantic-ui/definitions/modules/checkbox.js';
+import '../src/semantic-ui/definitions/modules/dropdown.js';
+import '../src/semantic-ui/semantic.less';
+
+$(document).on('turbolinks:load', function() {
+    $('.ui.dropdown').dropdown();
+
+    $(window).scroll(function () {
+        if (window.matchMedia("(max-width: 1024px)").matches && $(document).scrollTop() > 0) {
+            $('.main-header').addClass('u-fixed');
+        } else {
+            $('.main-header').removeClass('u-fixed');
+        }
+    });
+});
