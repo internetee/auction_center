3696b95c95c455334625bb69c379f619db760842
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2019-01-24 10:22:46 +0200

    Make sure that already cancelled invoices are not cancelled again

--- test/jobs/invoice_cancellation_job_test.rb
+++ test/jobs/invoice_cancellation_job_test.rb
@@ -7,10 +7,18 @@ class InvoiceCancellationJobTest < ActiveJob::TestCase
     @invoice = invoices(:orphaned)
   end
 
-  def test_overdue_invoices_are_canceled_automatically
+  def test_overdue_invoices_are_cancelled_automatically
     InvoiceCancellationJob.perform_now
 
     @invoice.reload
     assert_equal('cancelled', @invoice.status)
+  end
+
+  def test_paid_invoices_are_not_cancelled_automatically
+    @invoice.mark_as_paid_at(Time.zone.now)
+    InvoiceCancellationJob.perform_now
+
+    @invoice.reload
+    assert_equal('paid', @invoice.status)
   end
 end
