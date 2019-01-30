1bcd078bcf6dbded8a965d99b24e6f818ff6970a
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2019-01-30 16:07:33 +0200

    Add failed tests to check for phone confirmation

---
+++ test/system/email_confirmations_test.rb
@@ -0,0 +1,7 @@
+require 'application_system_test_case'
+
+class EmailConfirmationsTest < ApplicationSystemTestCase
+  def test_you_are_redirected_to_user_profile_after_confirmation
+    assert(false)
+  end
+end
---
+++ test/system/phone_confirmations_test.rb
@@ -0,0 +1,15 @@
+require 'application_system_test_case'
+
+class PhoneConfirmationsTest < ApplicationSystemTestCase
+  def test_phone_confirmation_is_sent_on_page_visit
+    assert(false)
+  end
+
+  def test_user_cannot_submit_an_offer_without_phone_confirmation
+    assert(false)
+  end
+
+  def test_user_must_confirm_phone_numbers_on_each_change
+    assert(false)
+  end
+end
