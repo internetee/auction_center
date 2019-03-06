cc0ef15aebce836a2f04afa756a451a2fa7b8271
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2019-03-06 11:49:32 +0200

    Test that sessions are expiring after 10 minutes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -163,7 +163,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length.
-  config.password_length = 6..128
+  config.password_length = 8..128
 
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
@@ -173,7 +173,7 @@ Devise.setup do |config|
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
-  # config.timeout_in = 30.minutes
+  config.timeout_in = 10.minutes
 
   # ==> Configuration for :lockable
   # Defines which strategy will be used to lock an account.
