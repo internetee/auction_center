db593d956df4b089c3f82332cd4f53a01dfdaf8c
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2019-03-13 13:28:11 +0200

    Add header to auctions#index action

--- test/integration/api_auctions_list_test.rb
+++ test/integration/api_auctions_list_test.rb
@@ -14,8 +14,8 @@ class ApiAuctionsListTest < ActionDispatch::IntegrationTest
   end
 
   def test_cors_preflight_check
+    # OPTIONS /auctions
     process(:options, auctions_path, headers: { 'Origin' => 'https://example.com' })
-
     assert_equal('https://example.com', response.headers['Access-Control-Allow-Origin'])
     assert_equal('GET, OPTIONS', response.headers['Access-Control-Allow-Methods'])
     assert_equal('Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, ' \
@@ -23,14 +23,18 @@ class ApiAuctionsListTest < ActionDispatch::IntegrationTest
                response.headers['Access-Control-Allow-Headers'])
     assert_equal('3600', response.headers['Access-Control-Max-Age'])
 
+    # OPTIONS /auctions.json
     process(:options, '/auctions.json', headers: { 'Origin' => 'https://example.com' })
-
     assert_equal('https://example.com', response.headers['Access-Control-Allow-Origin'])
     assert_equal('GET, OPTIONS', response.headers['Access-Control-Allow-Methods'])
     assert_equal('Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, ' \
                  'X-User-Token, X-User-Email',
                response.headers['Access-Control-Allow-Headers'])
     assert_equal('3600', response.headers['Access-Control-Max-Age'])
+
+    # GET /auctions
+    get auctions_path, headers: { 'Origin' => 'https://example.com', 'Accept' => 'application/json' }
+    assert_equal('https://example.com', response.headers['Access-Control-Allow-Origin'])
   end
 
   def test_can_get_a_list_of_current_auctions_as_json
@@ -49,6 +53,8 @@ class ApiAuctionsListTest < ActionDispatch::IntegrationTest
     expected_domains = ['with-offers.test', 'no-offers.test', 'with-invoice.test', 'orphaned.test']
     assert_equal(expected_domains.to_set, response_json.map { |e| e['domain_name'] }.to_set)
   end
+
+
 
   def test_cannot_get_a_list_of_current_auctions_as_xml
     assert_raises ActionController::UnknownFormat do
