199e7132b480a5d313400fe6af6bac60c4568533
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2019-03-29 16:39:06 +0200

    Handle VAT in Estonia according to business rules
    * For Estonian person, VAT should be applied always
    * For EU private person, VAT should be applied
    * For EU person with a VAT number, VAT should not be
      applied (chargeback)
    * For non-EU person, do not calculate VAT

--- app/models/billing_profile.rb
+++ app/models/billing_profile.rb
@@ -24,9 +24,12 @@ class BillingProfile < ApplicationRecord
   end
 
   def vat_rate
-    if vat_code.present?
+    if country_code == 'EE'
+      Countries.vat_rate_from_alpha2_code(country_code)
+    elsif vat_code.present?
       BigDecimal('0')
     else
+      # We only supply VAT values for EU countries, otherwise it is always 0.
       Countries.vat_rate_from_alpha2_code(country_code)
     end
   end
