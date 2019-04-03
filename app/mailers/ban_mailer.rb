e3144ce89dbcbb3b97873b91bae0b114c26ef242
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2019-04-03 13:32:52 +0300

    Make ban email depend on the number of overdue invoices
    As it stands, it is not in line with what is in the code. Actual fix
    should come with #198.

--- app/mailers/ban_mailer.rb
+++ app/mailers/ban_mailer.rb
@@ -4,6 +4,14 @@ class BanMailer < ApplicationMailer
     @user = @ban.user
     I18n.locale = @user.locale
 
+    invoices = Invoice.where(user: @user, status: Invoice.statuses[:cancelled]).count
+
+    @past_due_invoices = if I18n.locale == :en
+                           invoices.ordinalize
+                         else
+                           invoices
+                         end
+
     mail(to: @user.email, subject: t('.subject'))
   end
 end
