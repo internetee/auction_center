b7ec92a920c8571fb0203314ffb2c7d5942f0f69
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2019-05-20 14:15:27 +0300

    Add Estonian translation keys for Wishlist mailer

--- test/mailers/previews/wishlist_mailer_preview.rb
+++ test/mailers/previews/wishlist_mailer_preview.rb
@@ -7,4 +7,13 @@ class WishlistMailerPreview < ActionMailer::Preview
 
     WishlistMailer.auction_notification_mail(item, auction)
   end
+
+  def auction_notification_mail_estonian
+    user = User.new(email: "some@email.com", locale: :et)
+    auction = Auction.new(domain_name: 'example.test', ends_at: Time.zone.now,
+                          uuid: SecureRandom.uuid)
+    item = WishlistItem.new(user: user, domain_name: auction.domain_name)
+
+    WishlistMailer.auction_notification_mail(item, auction)
+  end
 end
