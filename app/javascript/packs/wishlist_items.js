cca86e5aef6e92dd6c765c352cc29a6178f00c04
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2019-05-21 09:57:51 +0300

    Move form up in the UI

--- app/javascript/packs/wishlist_items.js
+++ app/javascript/packs/wishlist_items.js
@@ -1,19 +1,25 @@
-document.addEventListener("ajax:error", function(event) {
+function createListItem(string, document) {
+    let listItem = document.createElement('li');
+    listItem.innerHTML = string;
+    return listItem;
+};
+
+document.addEventListener("ajax:error", (event) => {
+    let xhr, status, error;
     [xhr, status, error] = event.detail;
 
     let errorsBlock = document.getElementById('errors');
     let errorsList = document.getElementById('errors-list');
 
-    xhr.forEach(function(element) {
-        let listItem = document.createElement('li');
-        listItem.innerHTML = element;
+    xhr.forEach(function(message) {
+        let listItem = createListItem(message, document);
         errorsList.appendChild(listItem);
     });
 
     errorsBlock.classList.remove('hidden');
 });
 
-document.addEventListener("ajax:beforeSend", function(event) {
+document.addEventListener("ajax:beforeSend", (event) => {
     let errorsBlock = document.getElementById('errors');
     let errorsList = document.getElementById('errors-list');
 
