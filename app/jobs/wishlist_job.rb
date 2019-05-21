a6d15912ffa1168a8469ed103526b543e230223c
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2019-05-21 11:17:16 +0300

    Add tests for job class
    Not really that necessary, but let's have them.

--- app/jobs/wishlist_job.rb
+++ app/jobs/wishlist_job.rb
@@ -4,6 +4,7 @@ class WishlistJob < ApplicationJob
   def perform(domain_name, auction_remote_id)
     wishlist_items = WishlistItem.where(domain_name: domain_name)
     auction = Auction.active.find_by(remote_id: auction_remote_id)
+    return unless auction
 
     wishlist_items.each do |item|
       WishlistMailer.auction_notification_mail(item, auction).deliver_later
