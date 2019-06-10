215a2e93f0c884821663f9823ef08a15f83ba08e
Maciej Szlosarczyk <maciej.szlosarczyk@eestiinternet.ee>
2019-06-10 10:53:47 +0300

    Add admin sortable tables test

---
+++ test/system/admin/admin_sortable_test.rb
@@ -0,0 +1,33 @@
+require 'application_system_test_case'
+
+class AdminSortableTest < ApplicationSystemTestCase
+  def setup
+    @administrator = users(:administrator)
+
+    sign_in(@administrator)
+  end
+
+  def test_auctions_are_sortable
+    visit admin_auctions_path
+
+    assert("auctions are sortable" == :foo)
+  end
+
+  def test_users_are_sortable
+    visit admin_users_path
+
+    assert("users are sortable" == :foo)
+  end
+
+  def test_results_are_sortable
+    visit admin_results_path
+
+    assert("results are sortable" == :foo)
+  end
+
+  def test_invoices_are_sortable
+    visit admin_invoices_path
+
+    assert("invoices are sortable" == :foo)
+  end
+end
