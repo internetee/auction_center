8afeb6c1b2339f4f58cf1f25b65ad7ce703aabf4
Karl Erik Õunapuu <karlerik@kreative.ee>
2019-10-07 16:19:13 +0300

    Break domain regex into two lines

--- app/models/wishlist_item.rb
+++ app/models/wishlist_item.rb
@@ -13,7 +13,8 @@ class WishlistItem < ApplicationRecord
             format: {
               # allows 1 to 61 characters with Estonian diacritic signs and 1 to 61 character
               # of the top level domain.
-              with: /\A[a-z0-9\-\u00E4\u00F5\u00F6\u00FC\u0161\u017E]{1,61}\.[a-z0-9]{1,61}(\.[a-z0-9]{1,61})?\z/,
+              with: /\A[a-z0-9\-\u00E4\u00F5\u00F6\u00FC\u0161\u017E]{1,61}\.
+              [a-z0-9]{1,61}(\.[a-z0-9]{1,61})?\z/x,
             }
   validate :must_fit_in_wishlist_size, on: :create
   validate :valid_tld, on: :create
