231e8caa046c43f3cc5fbb22fc8ffb9d9bc93e0e
Karl Erik Õunapuu <karlerik@kreative.ee>
2019-10-15 14:41:41 +0300

    Fixed translations for auction newsletter

--- test/jobs/daily_broadcast_auctions_job_test.rb
+++ test/jobs/daily_broadcast_auctions_job_test.rb
@@ -1,12 +1,15 @@
 require 'test_helper'
 
 class DailyBroadcastAuctionsJobTest < ActiveJob::TestCase
+  include ActionMailer::TestHelper
   def setup
     super
 
     @participant = users(:participant)
     @participant.update!(auction_newsletter: true)
     travel_to Time.parse('2010-07-05 10:30 +0000').in_time_zone
+
+
   end
 
   def teardown
@@ -26,5 +29,15 @@ class DailyBroadcastAuctionsJobTest < ActiveJob::TestCase
     assert_equal(['user@auction.test'], email.to)
     assert_equal("Today's .EE domain auction list is available!", email.subject)
     assert_includes(email.body, "with-offers.test")
+  end
+
+  def test_includes_only_relevant_auctions
+    perform_enqueued_jobs do
+      DailyBroadcastAuctionsJob.perform_now
+    end
+
+    email = ActionMailer::Base.deliveries.last
+    assert_not_includes(email.body, "expired.test")
+    
   end
 end
