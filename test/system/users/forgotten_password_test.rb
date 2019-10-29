cb2df0289f407fd12f62d48167c436152e183a57
Alex Sherman <yul.golem@gmail.com>
2019-10-29 16:03:45 +0500

    I18n password reset form

--- test/system/users/forgotten_password_test.rb
+++ test/system/users/forgotten_password_test.rb
@@ -35,4 +35,30 @@ class ForgottenPasswordTest < ApplicationSystemTestCase
     click_link('Eesti keeles')
     assert(page.has_button?("Saada parooli muutmise juhised"))
   end
+
+  def test_password_reset_form_localized
+    visit new_user_password_path
+    new_password = 'new_password'
+
+    fill_in('user[email]', with: @user.email)
+    click_link_or_button 'Send me reset password instructions'
+    last_email = ActionMailer::Base.deliveries.last
+
+    # Match link from the email, for example:
+    # <p><a href=\"https://auction.example.com/sessions/password/edit?reset_password_token=c3vwRxLdSKot7HzTnVVh\">
+    match_token = last_email.body.match(/reset_password_token=((\w|-)+)/)
+    visit edit_user_password_path(reset_password_token: match_token[1])
+
+    assert(page.has_content?("New password"))
+    assert(page.has_content?("8 characters minimum"))
+    assert(page.has_content?("Confirm new password"))
+    assert(page.has_button?("Change my password"))
+
+    click_link('Eesti keeles')
+
+    assert(page.has_content?("Uus parool"))
+    assert(page.has_content?("Vähemalt 8 märki"))
+    assert(page.has_content?("Sisesta uus parool uuesti"))
+    assert(page.has_button?("Muuda mu parool"))
+  end
 end
