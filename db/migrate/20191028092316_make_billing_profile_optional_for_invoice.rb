5e897c0c284ff68cff242613e04adc1e0f25216b
Karl Erik Õunapuu <karlerik@kreative.ee>
2019-11-05 11:36:26 +0200

    Remove unused comments

--- db/migrate/20191028092316_make_billing_profile_optional_for_invoice.rb
+++ db/migrate/20191028092316_make_billing_profile_optional_for_invoice.rb
@@ -1,14 +1,10 @@
 class MakeBillingProfileOptionalForInvoice < ActiveRecord::Migration[5.2]
   def up
-    # change_column :offers, :billing_profile_id, :integer, null: true
-    # change_column :invoices, :billing_profile_id, :integer, null: true
     remove_foreign_key :invoices, :billing_profiles
     add_foreign_key :invoices, :billing_profiles, on_delete: :nullify
   end
 
   def down
-    # change_column :offers, :billing_profile_id, :integer, null: false
-    # change_column :invoices, :billing_profile_id, :integer, null: false
     remove_foreign_key :invoices, :billing_profiles
     add_foreign_key :invoices, :billing_profiles
   end
