a3823cfcec6ed5236558d3d65183c079c5279ca6
Alex Sherman <yul.golem@gmail.com>
2019-11-15 14:26:02 +0500

    Moved setting format validations to concern

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,13 +1,9 @@
 class Setting < ApplicationRecord
+  include Concerns::FormatValidator
   validates :code, presence: true
   validates :description, presence: true
   validates :value, presence: true
   validates :code, uniqueness: true
-  validate :validate_value
-
-  FORMAT_VALIDATIONS = {
-    terms_and_conditions_link: [:validate_json_hash],
-  }.with_indifferent_access.freeze
 
   def self.auction_currency
     Setting.find_by(code: :auction_currency).value
@@ -112,28 +108,5 @@ class Setting < ApplicationRecord
     rescue NoMethodError
       false
     end
-  end
-
-  private
-
-  def validate_value
-    methods = FORMAT_VALIDATIONS[code]
-    return unless methods
-
-    methods.each do |method|
-      send(method)
-    end
-  end
-
-  def validate_json_hash
-    return if parsable_json_hash?(value)
-
-    errors.add(:value, :invalid)
-  end
-
-  def parsable_json_hash?(value)
-    JSON.parse(value).is_a?(Hash)
-  rescue JSON::ParserError
-    errors.add(:value, :not_a_json_hash)
   end
 end
--- lib/tasks/data_migrations/convert_terms_and_conditions_link_setting.rake
+++ lib/tasks/data_migrations/convert_terms_and_conditions_link_setting.rake
@@ -7,9 +7,9 @@ namespace :data_migrations do
         Link to terms and conditions document. Must be single parsable hash of <locale>:<URL> elements.
         URL can be relative ('/public/terms_and_conditions.pdf') 
         or absolute ('https://example.com/terms_and_conditions.pdf'). Relative URL must start with a
-        forward slash. Default is: "{\"en\":\"https://example.com\"}"
+        forward slash. Default is: "{\"en\":\"https://example.com\", \"et\":\"https://example.et\"}"
       TEXT
-      value = "{\"en\":\"https://example.com\"}"
+      value = "{\"en\":\"https://example.com\", \"et\":\"https://example.et\"}"
       setting.update(value: value,
                      description: terms_and_conditions_description)
     end
