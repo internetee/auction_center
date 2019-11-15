398a5f51affbdbde89c1a3480f9bebf01de9cf73
Alex Sherman <yul.golem@gmail.com>
2019-11-15 16:23:22 +0500

    Add average bids

--- app/models/statistics_report.rb
+++ app/models/statistics_report.rb
@@ -2,7 +2,8 @@ class StatisticsReport
 
   attr_reader :start_date
   attr_reader :end_date
-  attr_accessor :auctions, :auctions_without_offers, :auctions_with_offers
+  attr_accessor :auctions, :auctions_without_offers, :auctions_with_offers,
+                :average_offers_per_auction
 
   def initialize(start_date:, end_date:)
     @start_date = start_date
@@ -21,6 +22,14 @@ class StatisticsReport
       @auctions[date] = auctions.count
       @auctions_with_offers[date] = auctions.with_offers.count
       @auctions_without_offers[date] = auctions.without_offers.count
+    end
+  end
+
+  def count_average_bids
+    (start_date..end_date).each do |date|
+      auctions = Auction.active_by_date(date).with_offers
+      total_offers = auctions.map{ |auction| auction.offers.count }.sum
+      @average_offers_per_auction[date] = total_offers / (auctions.count * 1.0)
     end
   end
 
