00269eb2643b2a713dac0becb41f461fceafd928
Alex Sherman <yul.golem@gmail.com>
2019-11-18 15:51:54 +0500

    Add basic StatisticsReport test

---
+++ test/models/statistics_report_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class StatisticsReportTest < ActiveSupport::TestCase
+
+  def setup
+    super
+    @start_date = Auction.first.starts_at.to_date
+    @end_date = Auction.last.ends_at.to_date
+  end
+
+  def test_gather_data_calls_required_methods
+    mock = Minitest::Mock.new
+
+    StatisticsReport.stub(:new, mock, [start_date: @start_date, end_date: @end_date]) do
+      mock.expect(:gather_data, {})
+      mock.expect(:count_auctions, {})
+      mock.expect(:count_average_bids, {})
+      mock.expect(:count_paid_unregistered_domains, {})
+      mock.expect(:count_unpaid_invoice_percentage, {})
+
+      summary_report = StatisticsReport.new(start_date: @start_date, end_date: @end_date)
+      summary_report.gather_data
+    end
+  end
+
+  def test_count_auctions
+    report = StatisticsReport.new(start_date: @start_date, end_date: @end_date)
+    report.gather_data
+    asserted_auctions_count = Auction
+                                .where('starts_at <= ? AND ends_at >= ?', @end_date, @end_date)
+                                .count
+    assert(report.auctions.is_a?(Hash))
+    assert_equal(report.auctions[@end_date], asserted_auctions_count)
+  end
+end
