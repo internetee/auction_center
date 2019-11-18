c9a5aa9795be1fae4fbba7985dcb76f542f0e8c2
Alex Sherman <yul.golem@gmail.com>
2019-11-18 13:23:05 +0500

    Add unregistered domains metric

--- app/models/statistics_report.rb
+++ app/models/statistics_report.rb
@@ -3,7 +3,7 @@ class StatisticsReport
   attr_reader :start_date
   attr_reader :end_date
   attr_accessor :auctions, :auctions_without_offers, :auctions_with_offers,
-                :average_offers_per_auction
+                :average_offers_per_auction, :unregistered_domains
 
   def initialize(start_date:, end_date:)
     @start_date = start_date
@@ -13,6 +13,7 @@ class StatisticsReport
 
   def gather_data
     count_auctions
+    count_average_bids
   end
 
   def count_auctions
@@ -30,6 +31,15 @@ class StatisticsReport
       auctions = Auction.active_by_date(date).with_offers
       total_offers = auctions.map{ |auction| auction.offers.count }.sum
       @average_offers_per_auction[date] = total_offers / (auctions.count * 1.0)
+    end
+  end
+
+  def count_paid_unregistered_domains
+    (start_date..end_date).each do |date|
+      results = Result.where(status: 'domain_not_registered')
+        .joins(:auction)
+        .where(auctions: { created_at: date })
+      @unregistered_domains[date] = results.count
     end
   end
 
