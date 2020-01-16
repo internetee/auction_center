bc92ad0c8b9acee68ffffee40c610ff4a11a1339
Alex Sherman <yul.golem@gmail.com>
2020-01-16 16:17:52 +0500

    Add test for custom validator

---
+++ test/models/concerns/safe_value_validator_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class SafeValueValidatorTest < ActiveSupport::TestCase
+
+  def setup
+    @user = User.new
+    @user.surname = '"><svg/onload=confirm(1)>'
+    @user.given_names = 'Given Names'
+  end
+
+  def test_false_on_invalid_values
+    validator = SafeValueValidator.new({attributes: {surname: @user.surname}})
+    assert_not(validator.validate_each(@user, :surname, @user.surname))
+  end
+
+  def test_true_on_valid_values
+    validator = SafeValueValidator.new({attributes: {given_names: @user.given_names}})
+    assert(validator.validate_each(@user, :given_names, @user.given_names))
+  end
+end
