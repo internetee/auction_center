fbce86667b58072cdd40807d8b3ae0e102753e4a
Alex Sherman <yul.golem@gmail.com>
2020-01-17 17:03:37 +0500

    Add validation check & notification for existing users

--- app/controllers/auth/sessions_controller.rb
+++ app/controllers/auth/sessions_controller.rb
@@ -1,7 +1,9 @@
 module Auth
   class SessionsController < Devise::SessionsController
     include BansHelper
+    include InvalidUserDataHelper
     after_action :set_ban_in_session, only: [:create]
+    after_action :set_invalid_data_flag_in_session, only: [:create]
 
     def create
       super
