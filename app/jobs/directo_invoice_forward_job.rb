d5c5916680a1996a2a83fd064792715eb79bcda0
Karl Erik Õunapuu <karlerik@kreative.ee>
2020-02-11 17:48:53 +0200

    Improve logging for DirectoInvoiceForwardJob

--- app/jobs/directo_invoice_forward_job.rb
+++ app/jobs/directo_invoice_forward_job.rb
@@ -12,35 +12,42 @@ class DirectoInvoiceForwardJob < ApplicationJob
       @client.invoices.add(generate_directo_invoice(invoice: invoice,
                                                     client: @client))
     end
-
+    logger.info "Attempting to send #{invoices.count} invoice(s) to Directo"
     sync_with_directo
   end
 
   def sync_with_directo
     res = @client.invoices.deliver(ssl_verify: false)
-    if res.code == '200'
-      update_invoice_directo_state(res.body)
-    else
-      logger.info("Directo responded with code #{res.code} instead of 200")
-    end
+    logger.info "Directo responded with code #{res.code}"
+    update_invoice_directo_state(res.body) if res.code == '200'
   rescue SocketError, Errno::ECONNREFUSED, Timeout::Error, Errno::EINVAL,
          Errno::ECONNRESET, EOFError, Net::HTTPBadResponse,
          Net::HTTPHeaderSyntaxError, Net::ProtocolError
-    logger.info('Exception when connecting to Directo')
+    logger.info('Network exception when connecting to Directo')
   end
 
   def update_invoice_directo_state(xml)
     pushed_invoices = []
     Nokogiri::XML(xml).css('Result').each do |result|
-      if result.attributes['Type'].value.to_i.zero?
-        pushed_invoices << result.attributes['docid'].value.to_i
-      end
+      inv_no = result.attributes['docid'].value.to_i
+      (pushed_invoices << inv_no) if invoice_reported_as_valid?(result)
     end
     mark_invoices_as_synced(invoice_ids: pushed_invoices)
   end
 
+  def invoice_reported_as_valid?(result)
+    inv_no = result.attributes['docid'].value.to_i
+    result_type = result.attributes['Type'].value.to_i
+    result_desc = result.attributes['Desc'].value
+    (return true) if result_type.zero?
+
+    logger.info "Not marking invoice ##{inv_no} as sent. Desc: #{result_desc}"
+    false
+  end
+
   def mark_invoices_as_synced(invoice_ids:)
     Invoice.where(number: invoice_ids).update(in_directo: true)
+    logger.info "Marked #{invoice_ids.count} invoices as synced to directo"
   end
 
   def generate_directo_invoice(invoice:, client:)
