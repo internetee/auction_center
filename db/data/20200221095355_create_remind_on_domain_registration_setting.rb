f81b3126bfbe526d37b521850e176238ad5ec2ef
Alex Sherman <yul.golem@gmail.com>
2020-02-21 14:57:23 +0500

    Add remind on domain registration setting migration

---
+++ db/data/20200221095355_create_remind_on_domain_registration_setting.rb
@@ -0,0 +1,22 @@
+class CreateRemindOnDomainRegistrationSetting < ActiveRecord::Migration[6.0]
+  def up
+    Setting.transaction do
+      remind_everyday_description = <<~TEXT.squish
+        Days remaining to the registration deadline that triggers daily reminder email until
+        deadline is reached or domain is registered. This is in addition
+        to domain_registration_reminder setting that send reminder just once. Default: 0
+      TEXT
+      value = '0'
+
+      setting = Setting.find_or_create_by(code: :domain_registration_daily_reminder)
+
+      setting.update(value: value,
+                     description: remind_everyday_description,
+                     value_format: 'integer')
+      puts "Remind on domain registration every day setting updated"
+    end
+  end
+
+  def down
+  end
+end
