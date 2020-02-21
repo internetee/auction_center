934a073e78087d53fa1a11c7567eec7c9b0c506d
Alex Sherman <yul.golem@gmail.com>
2020-02-21 14:09:23 +0500

    Add shared element properties migration

---
+++ db/data/20200221090738_create_shared_element_properties.rb
@@ -0,0 +1,38 @@
+class CreateSharedElementProperties < ActiveRecord::Migration[6.0]
+  def up
+    hash = {
+      voog_site_url: {
+        description: <<~TEXT.squish,
+          VOOG site from which to fetch localized footer elements. Defaults to https://www.internet.ee.
+        TEXT
+        value: 'https://www.internet.ee',
+        value_format: 'string',
+      },
+      voog_api_key: {
+        description: <<~TEXT.squish,
+          VOOG site API key. Required to fetch footer content.
+        TEXT
+        value: 'changeme',
+        value_format: 'string',
+      },
+      voog_site_fetching_enabled: {
+        description: <<~TEXT.squish,
+          Boolean whether to enable fetching & showing footer element from VOOG site. Defaults to false.
+        TEXT
+        value: 'false',
+        value_format: 'boolean',
+      },
+    }
+
+    Setting.transaction do
+      hash.each do |key, value_hash|
+        setting = Setting.find_or_create_by(code: key)
+        setting.update!(value: value_hash[:value], description: value_hash[:description])
+      end
+      puts 'VOOG site fetching settings are set'
+    end
+  end
+
+  def down
+  end
+end
