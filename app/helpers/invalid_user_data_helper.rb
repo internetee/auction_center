af9f8faba5654b276f94513edcc84f5ef6b8df61
Alex Sherman <yul.golem@gmail.com>
2020-05-25 15:32:42 +0500

    Add message banner if phone confirmation is not unique

--- app/helpers/invalid_user_data_helper.rb
+++ app/helpers/invalid_user_data_helper.rb
@@ -7,9 +7,14 @@ module InvalidUserDataHelper
 
   def invalid_data_banner
     return unless session['user.invalid_user_data']
-
-    content_tag(:div, class: 'ui message ban') do
-      content_tag(:div, t('users.invalid_user_data'), class: 'header')
+    if current_user.phone_number_was_already_confirmed?
+      content_tag(:div, class: 'ui message flash') do
+        content_tag(:div, t('already_confirmed'), class: 'header')
+      end
+    else
+      content_tag(:div, class: 'ui message ban') do
+        content_tag(:div, t('users.invalid_user_data'), class: 'header')
+      end
     end
   end
 
@@ -17,7 +22,7 @@ module InvalidUserDataHelper
 
   def user_data_invalid?
     current_user.invalid? &&
-      current_user.errors.keys.any? { |key| %i[given_names surname].include?(key) } ||
+      current_user.errors.keys.any? { |key| %i[given_names surname mobile_phone].include?(key) } ||
       (current_user.billing_profiles.present? && current_user.billing_profiles.any?(&:invalid?))
   end
 end
