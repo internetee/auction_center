925664d01b6db5e08d5b33fdc89e8c7852a03525
Alex Sherman <yul.golem@gmail.com>
2020-05-27 13:55:08 +0500

    Add data migration to revoke phone confirmation on previous users

--- db/data/20200527081406_fix_multiple_phone_confirmations.rb
+++ db/data/20200527081406_fix_multiple_phone_confirmations.rb
@@ -1,6 +1,15 @@
 class FixMultiplePhoneConfirmations < ActiveRecord::Migration[6.0]
   def up
-    binding.pry
+    grouped_users = User.with_confirmed_phone.order(created_at: :desc).group_by(&:mobile_phone)
+    grouped_users.each do |_key, value|
+      next if value.count == 1
+
+      value.each_with_index do |value, index|
+        next if index.zero?
+
+        value.update_column(:mobile_phone_confirmed_at, nil)
+      end
+    end
   end
 
   def down; end
