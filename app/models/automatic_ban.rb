62e5a2f362d22aa9fb1f238ffd132e1c9de3a815
Alex Sherman <yul.golem@gmail.com>
2021-03-11 16:12:05 +0500

    Make automatic bans long & based on number of unpaid invoices without bans

--- app/models/automatic_ban.rb
+++ app/models/automatic_ban.rb
@@ -4,13 +4,14 @@ class AutomaticBan
   # Minimum registration period is three months. Having short ban period
   # that uses the same time should abusers from participating in the most
   # immediate rounds of auctions for a domain name.
-  SHORT_BAN_PERIOD_IN_MONTHS = 3
+  BAN_PERIOD_IN_MONTHS = Setting.find_by(code: 'ban_length').retrieve
 
   attr_reader :invoice
   attr_reader :user
   attr_reader :domain_name
   attr_reader :ban
-  attr_reader :unpaid_invoices
+  attr_reader :unpaid_invoices_ids
+  attr_reader :existing_bans_ids
 
   def initialize(invoice:, user:, domain_name:)
     @invoice = invoice
@@ -19,9 +20,13 @@ class AutomaticBan
   end
 
   def create
-    @unpaid_invoices = Invoice.where(user_id: user, status: Invoice.statuses[:cancelled]).count
+    @unpaid_invoices_ids = Invoice.where(user_id: user, status: Invoice.statuses[:cancelled])
+                                  .pluck(:id)
+    @existing_bans_ids = Ban.where(invoice_id: @unpaid_invoices_ids).pluck(:id)
 
-    raise Errors::NoCancelledInvoices.new(user.id, domain_name) if unpaid_invoices.zero?
+    if @unpaid_invoices_ids.count == @existing_bans_ids.count
+      raise Errors::NoCancelledInvoices.new(user.id, domain_name)
+    end
 
     create_ban
     assign_ban_length
@@ -30,25 +35,19 @@ class AutomaticBan
   end
 
   def create_ban
-    @ban = Ban.new(user: user, valid_from: Time.zone.now.to_datetime, invoice: invoice)
+    @ban = Ban.new(user: user,
+                   valid_from: Time.zone.now.to_datetime,
+                   invoice: invoice,
+                   domain_name: domain_name)
   end
 
   def assign_ban_length
     now = Time.zone.now.to_datetime
-    setting = Setting.find_by(code: 'ban_number_of_strikes').retrieve
-    if unpaid_invoices < setting
-      @ban.update!(domain_name: domain_name, valid_from: now,
-                   valid_until: now >> SHORT_BAN_PERIOD_IN_MONTHS)
-    elsif unpaid_invoices >= setting
-      @ban.update!(valid_until: now >> Setting.find_by(code: 'ban_length').retrieve)
-    end
+    @ban.update!(valid_from: now,
+                 valid_until: now >> BAN_PERIOD_IN_MONTHS)
   end
 
   def send_email
-    if ban.domain_name
-      BanMailer.short_ban_mail(@ban, unpaid_invoices, @domain_name).deliver_later
-    else
-      BanMailer.long_ban_mail(@ban, unpaid_invoices, @domain_name).deliver_later
-    end
+    BanMailer.ban_mail(@ban, unpaid_invoices_ids.count, @domain_name).deliver_later
   end
 end
