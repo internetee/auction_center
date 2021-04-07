e518cf877e47fae1d1451432998af3d0c2911827
Alex Sherman <yul.golem@gmail.com>
2021-04-07 14:15:40 +0500

    Create invoice right after result creation

--- app/mailers/result_mailer.rb
+++ app/mailers/result_mailer.rb
@@ -4,7 +4,8 @@ class ResultMailer < ApplicationMailer
     @user = result.user
     @auction = result.auction
     I18n.locale = @user.locale
-    @linkpay_url = result&.invoice&.linkpay_url || result_url(@result.uuid)
+
+    @linkpay_url = result.invoice&.linkpay_url
 
     mail(to: @user.email, subject: t('.subject', domain_name: @auction.domain_name))
   end
