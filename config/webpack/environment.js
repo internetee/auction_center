b2d32d7dd5cb371fbec36e440e74381634b2d44f
dinsmol <dinar184@mail.ru>
2021-10-19 13:29:32 +0300

    fixed environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -16,4 +16,25 @@ environment.loaders.get('sass').use.splice(-1, 0, {
 });
 
 environment.loaders.append('less', less);
+
+const customConfig = {
+    resolve: {
+        fallback: {
+            dgram: false,
+            fs: false,
+            net: false,
+            tls: false,
+            child_process: false
+        }
+    }
+};
+
+environment.config.delete('node.dgram')
+environment.config.delete('node.fs')
+environment.config.delete('node.net')
+environment.config.delete('node.tls')
+environment.config.delete('node.child_process')
+
+environment.config.merge(customConfig);
+
 module.exports = environment;
