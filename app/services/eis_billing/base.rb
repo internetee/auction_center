af5ae316143b66796ec994b7f4d6a2e6d0e17184
olegphenomenon <oleg.hasjanov@eestiinternet.ee>
2022-03-23 12:18:33 +0200

    made migration and update token encoder

--- app/services/eis_billing/base.rb
+++ app/services/eis_billing/base.rb
@@ -1,24 +1,10 @@
 module EisBilling
   class Base
-    BASE_URL = ''
-    if Rails.env.staging?
-      BASE_URL = AuctionCenter::Application.config
-                                             .customization[:billing_system_integration]
-                                             &.compact&.fetch(:eis_billing_system_base_url_staging, '')
-    else
-      BASE_URL = AuctionCenter::Application.config
-                                             .customization[:billing_system_integration]
-                                             &.compact&.fetch(:eis_billing_system_base_url_dev, '')
-    end
+    BASE_URL = AuctionCenter::Application.config
+                                            .customization[:billing_system_integration]
+                                            &.compact&.fetch(:eis_billing_system_base_url, '')
 
-    INITIATOR = 'auction'
-
-    SECRET_WORD = AuctionCenter::Application.config
-                                              .customization[:billing_system_integration]
-                                              &.compact&.fetch(:secret_word, '')
-    SECRET_ACCESS_WORD = AuctionCenter::Application.config
-                                                     .customization[:billing_system_integration]
-                                                     &.compact&.fetch(:secret_access_word, '')
+    INITIATOR = 'auction'.freeze
 
     def self.base_request(url:)
       uri = URI(url)
@@ -33,11 +19,11 @@ module EisBilling
     end
 
     def self.generate_token
-      JWT.encode(payload, SECRET_WORD )
+      JWT.encode(payload, billing_secret)
     end
 
     def self.payload
-      { data: SECRET_ACCESS_WORD }
+      { initiator: INITIATOR }
     end
 
     def self.headers
@@ -45,6 +31,10 @@ module EisBilling
       'Authorization' => "Bearer #{generate_token}",
       'Content-Type' => 'application/json',
       }
+    end
+
+    def self.billing_secret
+      AuctionCenter::Application.config.customization[:billing_system_integration]&.compact&.fetch(:billing_secret, '')
     end
   end
 end
