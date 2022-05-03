aecb95fce5a9851ac5e059052cbf30bd7512d6de
olegphenomenon <oleg.hasjanov@eestiinternet.ee>
2022-05-03 16:36:56 +0300

    added turbo handler for devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,8 +1,30 @@
 # frozen_string_literal: true
+class TurboFailureApp < Devise::FailureApp
+  def respond
+    if request_format == :turbo_stream
+      redirect
+    else
+      super
+    end
+  end
+
+  def skip_format?
+    %w(html turbo_stream */*).include? request_format.to_s
+  end
+end
 
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+
+
+config.navigational_formats = ['*/*', :html, :turbo_stream]
+
+config.warden do |manager|
+  manager.failure_app = TurboFailureApp
+#   manager.intercept_401 = false
+#   manager.default_strategies(scope: :user).unshift :some_external_strategy
+end
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
