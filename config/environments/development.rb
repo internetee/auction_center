ca10ee5e51577bdc62d57160c38ca709c389887a
olegphenomenon <oleg.hasjanov@eestiinternet.ee>
2022-05-04 14:07:42 +0300

    added later delay for rendering sockets data

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,7 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+  config.hosts = nil
 
   # Show full error reports.
   config.consider_all_requests_local = true
@@ -29,6 +30,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
+  config.action_cable.url = "ws://0.0.0.0:3000/cable"
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
