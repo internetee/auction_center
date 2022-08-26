5676537ac8ba9fab27fe164eed8cad58cb0c56e3
olegphenomenon <oleg.hasjanov@eestiinternet.ee>
2022-08-26 14:24:02 +0300

    added directo job tests

--- app/jobs/directo_invoice_forward_job.rb
+++ app/jobs/directo_invoice_forward_job.rb
@@ -1,10 +1,9 @@
 class DirectoInvoiceForwardJob < ApplicationJob
   def perform
     collected_data = []
-
     invoices = Invoice.where(status: 'paid', in_directo: false)
     return unless invoices.any?
-
+    
     invoices.each do |i|
       collected_data << i.as_directo_json
     end
