71b354ad74223666ae99478bc76f4ec4b98b4084
olegphenomenon <oleg.hasjanov@eestiinternet.ee>
2022-12-20 09:44:55 +0200

    update ordering

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -2,10 +2,11 @@ class HistoriesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    sort_column = params[:sort].presence_in(%w[domain name]) || 'id'
-    sort_direction = params[:direction].presence_in(%w[asc desc]) || 'desc'
+    # sort_column = params[:sort].presence_in(%w[domain_name starts_at ends_at]) || 'id'
+    # sort_direction = params[:direction].presence_in(%w[asc desc]) || 'desc'
+    # .order(sort_column => sort_direction)
 
-    auctions = Auction.where('ends_at <= ?', Time.zone.now).search(params).order(sort_column => sort_direction)
+    auctions = Auction.where('ends_at <= ?', Time.zone.now).search(params)
 
     @pagy, @auctions = pagy(auctions, items: params[:per_page] ||= 20, link_extra: 'data-turbo-action="advance"')
   end
