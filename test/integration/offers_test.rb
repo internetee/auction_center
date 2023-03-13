735b2c8f442f213c4f44c492f8afb110379f13bb
olegphenomenon <oleg.hasjanov@eestiinternet.ee>
2023-03-13 13:07:01 +0200

    added tests

--- test/integration/offers_test.rb
+++ test/integration/offers_test.rb
@@ -32,6 +32,36 @@ class OffersIntegrationTest < ActionDispatch::IntegrationTest
     assert_equal @auction.offers.first.user, @user
   end
 
+  def test_banned_user_cannot_create_an_offer
+    @auction.offers.destroy_all
+    @auction.reload
+    assert @auction.offers.empty?
+
+    valid_from = Time.parse('2010-07-05 10:30 +0000').in_time_zone
+    Ban.create!(user: @user,
+                domain_name: @auction.domain_name,
+                valid_from: valid_from, valid_until: valid_from + 3.days)
+
+    @user.reload
+    assert @user.banned?
+
+    params = {
+      offer: {
+        auction_id: @auction.id,
+        user_id: @user.id,
+        price: 7.0,
+        billing_profile_id: @user.billing_profiles.first.id
+      }
+    }
+
+    post auction_offers_path(auction_uuid: @auction.uuid),
+                                     params: params,
+                                     headers: {}
+    
+    assert_equal flash[:alert], I18n.t('.offers.create.ban')
+    assert @auction.offers.empty?
+  end
+
   def test_another_user_can_create_a_bid
     params = {
       offer: {
