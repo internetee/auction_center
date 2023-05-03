4314908a7593c099d6fb07ba31210eda3b92b811
olegphenomenon <oleg.hasjanov@eestiinternet.ee>
2023-05-03 09:47:26 +0300

    restore pay all btn

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -51,8 +51,6 @@ class InvoicesController < ApplicationController
   end
 
   def pay_all_bills
-    return
-
     issued_invoices = invoices_list_by_status(Invoice.statuses[:issued])
     response = EisBilling::BulkInvoicesService.call(invoices: issued_invoices,
                                                     customer_url: linkpay_callback_url)
