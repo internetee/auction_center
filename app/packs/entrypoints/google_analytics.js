28b028982925b37668f9f86ec67713737bf6013d
Sergei Tsoganov <sergei@Sergeis-MacBook-Pro-2.local>
2023-08-18 19:44:09 +0300

    Fixed google analytics

--- app/packs/entrypoints/google_analytics.js
+++ app/packs/entrypoints/google_analytics.js
@@ -1,4 +1,4 @@
-document.addEventListener('turbolinks:load', (event) => {
+document.addEventListener('turbo:load', (event) => {
     const googleId = document
         .getElementById('google-tracking-id')
         .getAttribute('data-value');
@@ -9,12 +9,10 @@ document.addEventListener('turbolinks:load', (event) => {
             dataLayer.push(arguments);
         };
         gtag('js', new Date());
-        gtag('config', googleId);
-
         gtag('config', googleId, {
-            'page_location': event.data.url,
-            'cookie_prefix': 'AuctionTest',
-            'cookie_domain': 'auction.ee',
+            page_location: event.detail.url,
+            cookie_prefix: 'AuctionTest',
+            cookie_domain: 'auction.ee',
         });
     };
 });
