e971975b582bb6903cda805f8adcc914b3cf6295
Oleg Hasjanov <oleg.phenomenon@gmail.com>
2023-08-31 11:38:23 +0300

    added claim domen modal window

---
+++ app/components/modals/winner_domain/component.rb
@@ -0,0 +1,34 @@
+module Modals
+  module WinnerDomain
+    class Component < ApplicationViewComponent
+      attr_reader :result
+
+      def initialize(result:)
+        super
+
+        @result = result
+      end
+
+      def registration_code_or_information
+        case result.status
+        when Result.statuses[:payment_received]
+          result.registration_code
+        when Result.statuses[:domain_registered]
+          I18n.t('.domain_already_registered')
+        else
+          I18n.t('.only_available_after_paying_invoice')
+        end
+      end
+
+      def pay_invoice_before
+        if result.payment_received?
+          I18n.t('.paid')
+        elsif result&.invoice
+          result.invoice.due_date
+        else
+          I18n.t('invoices.is_being_generated')
+        end
+      end
+    end
+  end
+end
