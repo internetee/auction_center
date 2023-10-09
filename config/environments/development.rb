bc346db1b17cde85f20625c170897ca84f878055
Oleg Hasjanov <oleg.phenomenon@gmail.com>
2023-10-09 14:50:32 +0300

    added password changed page, updated styles

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,6 +50,11 @@ Rails.application.configure do
   config.active_record.verbose_query_logs = true
 
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = {
+    host: 'localhost:3000',
+    protocol: 'http'
+  }
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
