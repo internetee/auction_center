669214d948c970f613b6de22b4777abb23a5aa03
Oleg Hasjanov <oleg.phenomenon@gmail.com>
2023-12-14 13:33:17 +0200

    fixed componen priview missing file

--- app/components/application_view_component_preview.rb
+++
@@ -1,5 +0,0 @@
-class ApplicationViewComponentPreview < ViewComponentContrib::Preview::Base
-  self.abstract_class = true
-
-  layout 'component_preview'
-end
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -6,4 +6,11 @@ const application = Application.start()
 application.debug = document.documentElement.classList.contains("debug");
 window.Stimulus   = application
 
+// if (process.env.RAILS_ENV === "test") {
+//   // propagate errors that happen inside Stimulus controllers
+//   application.handleError = (error, message, detail) => {
+//     throw error
+//   }
+// }
+
 export { application }
