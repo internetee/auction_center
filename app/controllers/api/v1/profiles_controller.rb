75b6fd48319b2b47813f603a57567ac9c36b4245
Oleg Hasjanov <oleghasjanov@MacBook-Pro-Oleg.local>
2024-02-16 13:17:43 +0200

    added endpoints for create user and billing profile

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class ProfilesController < ApplicationController
-      before_action :authenticate_user!
+      before_action :authenticate_user!, only: %i[update]
       respond_to :json
 
       skip_before_action :verify_authenticity_token
@@ -13,6 +13,30 @@ module Api
           Rails.logger.info @user.errors.inspect
           render json: current_user.errors, status: :unprocessable_entity
         end
+      end
+
+      def create
+        puts '----'
+        puts params_for_create
+        puts '----'
+
+        user = User.new(params_for_create)
+
+        if user.save
+          sign_in(User, user)
+          render json: user, status: :created
+        else
+          Rails.logger.info user.errors.inspect
+          render json: user.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def params_for_create
+        params.require(:user).permit(:email, :password, :password_confirmation, :country_code,
+                                     :given_names, :surname, :mobile_phone, :accepts_terms_and_conditions,
+                                     :locale, :daily_summary, :identity_code)
       end
 
       def params_for_update
