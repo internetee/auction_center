e548e094914a252f16ef0a748a908eea9809efd8
Oleg Hasjanov <oleghasjanov@MacBook-Pro-Oleg.local>
2024-02-16 13:17:43 +0200

    added endpoints for create user and billing profile

--- app/controllers/api/v1/billing_profiles_controller.rb
+++ app/controllers/api/v1/billing_profiles_controller.rb
@@ -6,6 +6,8 @@ module Api
 
       skip_before_action :verify_authenticity_token
 
+      rescue_from ActiveRecord::RecordNotUnique, with: :handle_unique_violation
+
       def index
         @billing_profiles = current_user.billing_profiles
 
@@ -22,11 +24,25 @@ module Api
         end
       end
 
+      def create
+        @billing_profile = current_user.billing_profiles.new(billing_profile_params)
+
+        if @billing_profile.save
+          render json: { billing_profile: @billing_profile }
+        else
+          render json: { errors: @billing_profile.errors }, status: 422
+        end
+      end
+
       private
 
       def billing_profile_params
         params.require(:billing_profile).permit(:name, :vat_code, :street, :city, :state, :postal_code,
                                                 :alpha_two_country_code, :uuid)
+      end
+
+      def handle_unique_violation
+        render json: { errors: 'A billing profile with the same VAT code for this user already exists.' }, status: 422
       end
     end
   end
