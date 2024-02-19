1a92c4f818130db063ae03ca168d7c6272c95189
Oleg Hasjanov <oleghasjanov@MacBook-Pro-Oleg.local>
2024-02-19 11:01:52 +0200

    move validation captcha to the form

--- app/controllers/phone_confirmations/send_sms_controller.rb
+++ app/controllers/phone_confirmations/send_sms_controller.rb
@@ -4,18 +4,10 @@ class PhoneConfirmations::SendSmsController < ApplicationController
   before_action :authorize_user
 
   include RecaptchaValidatable
-  recaptcha_action 'phone_confirmation'
+  recaptcha_action 'send_sms'
 
   def create
-    Rails.logger.info("PhoneConfirmations::SendSmsController#create: params=#{params.inspect}")
-    Rails.logger.info('----------')
-    Rails.logger.info(current_user.allow_to_send_sms_again?)
-    Rails.logger.info(recaptcha_valid)
-    Rails.logger.info(AuctionCenter::Application.config.customization[:mobile_sms_sent_time_limit_in_minutes].to_i.minutes)
-    Rails.logger.info(PhoneConfirmation::TIME_LIMIT.ago)
-    Rails.logger.info('----------')
-
-    if current_user.allow_to_send_sms_again?
+    if current_user.allow_to_send_sms_again? && recaptcha_valid
       PhoneConfirmationJob.perform_now(@phone_confirmation.user.id)
 
       flash[:notice] = I18n.t('phone_confirmations.create.sms_sent')
