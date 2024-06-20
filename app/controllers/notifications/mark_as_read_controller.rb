b8eeb2315b13a39761449bf5728d4929c644f8a2
oleghasjanov <oleg.phenomenon@gmail.com>
2024-06-20 11:33:15 +0300

    refactored-notifications

--- app/controllers/notifications/mark_as_read_controller.rb
+++ app/controllers/notifications/mark_as_read_controller.rb
@@ -1,8 +1,12 @@
-class Notifications::MarkAsReadController < ApplicationController
-  def update
-    current_user&.notifications&.unread&.map(&:mark_as_read!)
+# frozen_string_literal: true
 
-    flash[:notice] = t('notifications.all_marked_as_read')
-    redirect_to notifications_path, status: :see_other
+module Notifications
+  class MarkAsReadController < ApplicationController
+    def update
+      current_user&.notifications&.unread&.each(&:mark_as_read!)
+
+      flash[:notice] = t('notifications.all_marked_as_read')
+      redirect_to notifications_path, status: :see_other
+    end
   end
 end
