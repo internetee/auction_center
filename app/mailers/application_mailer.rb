1930ec2493dbf3315d025a8c9cee2a2c0ac65a9e
oleghasjanov <oleg.phenomenon@gmail.com>
2024-06-26 14:45:53 +0300

    remove test

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -12,8 +12,20 @@ class ApplicationMailer < ActionMailer::Base
   end
 
   def convert_email_to_punycode
+    convert_recipient_to_punycode
+    convert_carbon_copy_to_punycode
+    convert_blind_carbon_copy_to_punycode
+  end
+
+  def convert_recipient_to_punycode
     mail.to = Array(mail.to).map { |email| convert_domain_to_punycode(email) }
+  end
+
+  def convert_carbon_copy_to_punycode
     mail.cc = Array(mail.cc).map { |email| convert_domain_to_punycode(email) }
+  end
+
+  def convert_blind_carbon_copy_to_punycode
     mail.bcc = Array(mail.bcc).map { |email| convert_domain_to_punycode(email) }
   end
 
--- test/integration/users_test.rb
+++ test/integration/users_test.rb
@@ -22,31 +22,4 @@ class UsersIntegrationTest < ActionDispatch::IntegrationTest
                                                      password_confirmation: new_password } }
     end
   end
-
-  # def test_user_create_with_punicode
-  #   assert_enqueued_emails 1 do
-  #     post users_path, params: { 
-  #       user: { 
-  #         email: 'example@ää.eu',
-  #         given_names: 'John',
-  #         surname: 'Doe',
-  #         password: 'password123',
-  #         password_confirmation: 'password123',
-  #         terms_and_conditions_accepted_at: Time.now.utc,
-  #         locale: 'en',
-  #         mobile_phone: '+37269900366',
-  #         identity_code: '60001017793',
-  #         country_code: 'EE',
-  #         accepts_terms_and_conditions: true,
-  #         roles: ['participant'],
-  #         phone_number_confirmed: true,
-  #         provider: 'email'}
-  #     }
-  #   end
-
-  #   punicode = SimpleIDN.to_ascii('ää.eu')
-  #   assert User.find_by(email: "example@#{punicode}")
-
-  #   assert_redirected_to new_user_session_path
-  # end
 end
