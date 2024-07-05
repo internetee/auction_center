0e0361a475db2f18201587a8c30e1fc7771d2f1f
oleghasjanov <oleg.phenomenon@gmail.com>
2024-07-05 12:41:48 +0300

    removed entrypoint webpacker js code

--- app/packs/entrypoints/controllers/hello_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-export default class extends Controller {
-  initialize() {
-  }
-  connect() {
-    this.element.textContent = "Hello World!"
-  }
-}
--- app/packs/entrypoints/payment_orders.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('turbo:load', function() {
-    setTimeout(function() {
-        const form = document.getElementById('payment-order-form');
-        if (form) {
-            form.submit();
-        }
-    }, 1000);
-});
