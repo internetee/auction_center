d4d030ddeb56be5d9abecf2cb8ca17eafdf1576a
oleghasjanov <oleg.phenomenon@gmail.com>
2024-07-15 12:41:55 +0300

    added termd of condition to header and footer

--- app/components/pages/auction/cards/component.rb
+++ app/components/pages/auction/cards/component.rb
@@ -1,7 +1,23 @@
 module Pages
   module Auction
     module Cards
-      class Component < ApplicationViewComponent; end
+      class Component < ApplicationViewComponent
+        def faq_link
+          if I18n.locale == :et
+            'https://www.internet.ee/abi-ja-info/kkk#III__ee_domeenioksjonid'
+          elsif I18n.locale == :en
+            'https://www.internet.ee/help-and-info/faq#III__ee_domain_auctions'
+          end
+        end
+
+        def reserved_domain_names_link
+          if I18n.locale == :en
+            'https://www.internet.ee/eif/news/auction-schedule-of-reserved-domains'
+          elsif I18n.locale == :et
+            'https://www.internet.ee/eis/uudised/reserveeritud-domeenide-oksjonite-kava'
+          end
+        end
+      end
     end
   end
 end
