ea788ba8b91de4db3e6909e5c87d344c245f4d6d
oleghasjanov <oleg.phenomenon@gmail.com>
2025-06-20 10:38:37 +0300

    added api healthcheck

--- app/controllers/api/v1/api_healthcare_controlle.rb
+++
@@ -1,9 +0,0 @@
-class Api::V1::ApiHealthcareController < ApplicationController
-  def index
-    if Feature.mobile_api_enabled?
-      render json: { message: 'API is healthy' }
-    else
-      render json: { message: 'API is turned off' }, status: 403
-    end
-  end
-end
