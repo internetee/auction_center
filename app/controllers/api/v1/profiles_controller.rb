a6752c8578541b3b46e110c54b7048a5e0ad9569
oleghasjanov <oleg.phenomenon@gmail.com>
2025-07-21 13:05:39 +0300

    fixes

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -10,29 +10,38 @@ module Api
         if current_user.update(params_for_update)
           render json: current_user, status: :ok
         else
-          Rails.logger.info @user.errors.inspect
-          render json: current_user.errors, status: :unprocessable_entity
+          Rails.logger.info current_user.errors.inspect
+          render json: { errors: user.errors.to_hash(true) }, status: :unprocessable_entity
         end
       end
 
       def create
-        user = User.new(params_for_create)
+        user = initialize_user(params_for_create)
+        set_locale(user)
 
         if user.save
           sign_in(User, user)
           render json: user, status: :created
         else
           Rails.logger.info user.errors.inspect
-          render json: user.errors, status: :unprocessable_entity
+          render json: { errors: user.errors.to_hash(true) }, status: :unprocessable_entity
         end
       end
 
       private
 
+      def initialize_user(user)
+        User.new(params_for_create)
+      end
+
       def params_for_create
         params.require(:user).permit(:email, :password, :password_confirmation, :country_code,
                                      :given_names, :surname, :mobile_phone, :accepts_terms_and_conditions,
                                      :locale, :daily_summary, :identity_code)
+      end
+      
+      def set_locale(user)
+        I18n.locale = user.locale || I18n.default_locale
       end
 
       def params_for_update
