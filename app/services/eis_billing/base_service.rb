3f0c5300029cc45afcccf95137a3505b8843a66e
oleghasjanov <oleg.phenomenon@gmail.com>
2025-11-13 11:41:13 +0200

    added metrics for failed invoices

--- app/services/eis_billing/base_service.rb
+++ app/services/eis_billing/base_service.rb
@@ -4,12 +4,24 @@ module EisBilling
   module BaseService
     def struct_response(response)
       if response['error'].present?
+        increment_failure_counter(error_type: 'provider_error')
         wrap(result: false, instance: nil, errors: response['error'])
       else
         wrap(result: true, instance: response, errors: nil)
       end
     rescue StandardError => e
+      increment_failure_counter(error_type: 'exception', exception_class: e.class.name)
       wrap(result: false, instance: nil, errors: e)
+    end
+
+    private
+
+    def increment_failure_counter(error_type:, exception_class: nil)
+      Yabeda.eis_billing.payment_failures_total.increment(
+        service: self.class.name.demodulize,
+        error_type: error_type,
+        exception_class: exception_class || 'none'
+      )
     end
 
     def wrap(**kwargs)
