16bf9d6e098567dd941facacd0e3d2d83a3a1cb7
oleghasjanov <oleg.phenomenon@gmail.com>
2025-11-14 13:36:11 +0200

    fixing size

--- app/components/pages/offers/offers_table/component.rb
+++ app/components/pages/offers/offers_table/component.rb
@@ -11,14 +11,17 @@ module Pages
         end
 
         def header_collection
-          [{ column: 'auctions.domain_name', caption: t('auctions.domain_name'), options: { class: 'sorting' } },
-           { column: 'auctions.platform', caption: t('auctions.type'), options: { class: 'sorting' } },
+          [{ column: 'auctions.domain_name', caption: t('auctions.domain_name'),
+             options: { class: 'sorting' } },
+           { column: 'auctions.platform', caption: t('auctions.type'),
+             options: { class: 'sorting', style: 'width: 30px !important;' } },
            { column: nil, caption: t('offers.show.your_last_offer'), options: { class: '' } },
-           { column: 'offers.created_at', caption: t('offers.created_at'), options: { class: 'sorting' } },
+           { column: 'offers.created_at', caption: t('offers.created_at'),
+             options: { class: 'sorting' } },
            { column: 'auctions.ends_at', caption: t('auctions.ends_at'),
              options: { class: 'sorting' } },
            { column: nil, caption: t('result_name'), options: {} },
-           { column: nil, caption: t('auctions.offer_actions'), options: {} }]
+           { column: nil, caption: t('auctions.offer_actions'), options: { style: 'width: 150px !important;' } }]
         end
       end
     end
