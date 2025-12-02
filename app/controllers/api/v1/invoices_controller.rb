f28ce843eaa1d304aa8ab3362463319ba1ea15ed
oleghasjanov <oleg.phenomenon@gmail.com>
2025-12-02 13:35:15 +0200

    added domain name in json response of invoice endpoint

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -14,10 +14,10 @@ module Api
         @deposit_paid = current_user.domain_participate_auctions.includes(:auction).order(created_at: :desc)
 
         render json: {
-          issued_invoices: @issued_invoices,
-          paid_invoices: @paid_invoices,
-          cancelled_payable_invoices: @cancelled_payable_invoices,
-          cancelled_expired_invoices: @cancelled_expired_invoices,
+          issued_invoices: invoices_with_auction_name(@issued_invoices),
+          paid_invoices: invoices_with_auction_name(@paid_invoices),
+          cancelled_payable_invoices: invoices_with_auction_name(@cancelled_payable_invoices),
+          cancelled_expired_invoices: invoices_with_auction_name(@cancelled_expired_invoices),
           deposit_paid: @deposit_paid.map do |dpa|
             {
               id: dpa.id,
@@ -25,7 +25,8 @@ module Api
               status: dpa.status,
               invoice_number: dpa.invoice_number,
               domain_name: dpa.auction.domain_name,
-              auction_id: dpa.auction_id
+              auction_id: dpa.auction_id,
+              auction_name: dpa.auction.domain_name
             }
           end
         }
@@ -35,9 +36,16 @@ module Api
 
       def invoices_list_by_status(status)
         Invoice.accessible_by(current_ability)
+               .includes(result: :auction)
                .where(user_id: current_user.id)
                .where(status:)
                .order(due_date: :desc)
+      end
+
+      def invoices_with_auction_name(invoices)
+        invoices.map do |invoice|
+          invoice.as_json.merge(auction_name: invoice.result&.auction&.domain_name)
+        end
       end
     end
   end
