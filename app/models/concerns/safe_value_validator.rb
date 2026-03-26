c8dfb0d3690b5affc5902293e34cf2570b9dfc0a
oleghasjanov <oleg.phenomenon@gmail.com>
2026-03-26 13:31:58 +0200

    fixed test

--- app/models/concerns/safe_value_validator.rb
+++ app/models/concerns/safe_value_validator.rb
@@ -1,10 +1,12 @@
 class SafeValueValidator < ActiveModel::EachValidator
+  RIGHT_SINGLE_QUOTE = "\u2019"
+
   def validate_each(record, attribute, value)
     return true if value.blank?
 
     # Regexp for latin characters lower/upper-case
     unicode_chars = /\p{Latin}/
-    if %r{[^a-zA-Z#{unicode_chars.source}\/\d\s\/\'\'\-\.\,]}.match?(value)
+    if %r{[^a-zA-Z#{unicode_chars.source}\/\d\s\/\'\'\-\.\,#{RIGHT_SINGLE_QUOTE}]}.match?(value)
       record.errors.add(attribute, I18n.t('.value_is_not_safe'))
       return false
     end
